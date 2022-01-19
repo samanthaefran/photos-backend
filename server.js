7eaae353e4c2ecf2cf1a2830ec8e74f1e7f6099a
samantha frangi <sefrangi13@gmail.com>
2022-01-19 00:13:47 -0500

    adds index and create routes successfully

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const { PORT = 3001, DATABASE } = process.env
+const { PORT = 3001, DATABASE_URL } = process.env
 
 const express = require('express')
 
@@ -11,7 +11,9 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const morgan = require('morgan')
 
-const PhotosSchema = new mongoose.Schema ({
+mongoose.connect(DATABASE_URL)
+
+const PhotosSchema = new mongoose.Schema({
   title: String,
   url: String,
   description: String,
@@ -23,8 +25,24 @@ app.use(cors())
 app.use(morgan("dev"))
 app.use(express.json())
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send('i work!')
+})
+
+app.get("/photos", async (req, res) => {
+  try {
+    res.json(await Photos.find({}))
+  } catch (error) {
+    res.status(400).json(error)
+  }
+})
+
+app.post("/photos", async (req, res) => {
+  try {
+    res.jason(await Photos.create(req.body))
+  } catch(error) {
+    res.status(400).json(error)
+  }
 })
 
 app.listen(PORT, () => console.log(`${PORT}`))
